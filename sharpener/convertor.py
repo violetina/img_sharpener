c182a8853fa50faac3bb3ee8760ce3081f36ac83
Tim Bowers <djsmiley2k@gmail.com>
2016-03-21 19:33:15 +0000

    Update convertor.py

--- sharpener/convertor.py
+++ sharpener/convertor.py
@@ -2,8 +2,8 @@ from subprocess import check_output, STDOUT
 
 
 
-def convert(source_file, destination_file ):
+def convert(source_file, destination_file, amount ):
     output = check_output(["convert",
                            source_file,
-                           "-sharpen","0x1",
+                           "-sharpen", amount,
                            destination_file], stderr=STDOUT)
